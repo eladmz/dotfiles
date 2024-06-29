7c06a9f3a47f505bdc3ada204f96996a268f0d66
eladmz <eladmz@gmail.com>
2024-06-30 01:58:31 +0300

    add hidden files and change qflist in telescope

--- .config/nvim/lua/eladmz/plugins/telescope.lua
+++ .config/nvim/lua/eladmz/plugins/telescope.lua
@@ -17,7 +17,45 @@ return {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+					},
+				},
+				vimgrep_arguments = {
+					"rg",
+					"--follow", -- Follow symbolic links
+					"--hidden", -- Search for hidden files
+					"--no-heading", -- Don't group matches by each file
+					"--with-filename", -- Print the file path with the matched lines
+					"--line-number", -- Show line numbers
+					"--column", -- Show column numbers
+					"--smart-case", -- Smart case search
+
+					-- Exclude some patterns from search
+					"--glob=!**/.git/*",
+					"--glob=!**/.idea/*",
+					"--glob=!**/.vscode/*",
+					"--glob=!**/build/*",
+					"--glob=!**/dist/*",
+					"--glob=!**/yarn.lock",
+					"--glob=!**/package-lock.json",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					-- needed to exclude some files & dirs from general search
+					-- when not included or specified in .gitignore
+					find_command = {
+						"rg",
+						"--files",
+						"--hidden",
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/build/*",
+						"--glob=!**/dist/*",
+						"--glob=!**/yarn.lock",
+						"--glob=!**/package-lock.json",
 					},
 				},
 			},
