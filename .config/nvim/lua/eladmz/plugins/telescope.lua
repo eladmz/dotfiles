a7dfb834e024bb4e932ecdea24e887bcb275e34b
eladmz <eladmz@gmail.com>
2024-06-30 22:26:16 +0300

    add fzf git and some more colors

--- .config/nvim/lua/eladmz/plugins/telescope.lua
+++ .config/nvim/lua/eladmz/plugins/telescope.lua
@@ -20,44 +20,6 @@ return {
 						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
 				},
-				vimgrep_arguments = {
-					"rg",
-					"--follow", -- Follow symbolic links
-					"--hidden", -- Search for hidden files
-					"--no-heading", -- Don't group matches by each file
-					"--with-filename", -- Print the file path with the matched lines
-					"--line-number", -- Show line numbers
-					"--column", -- Show column numbers
-					"--smart-case", -- Smart case search
-
-					-- Exclude some patterns from search
-					"--glob=!**/.git/*",
-					"--glob=!**/.idea/*",
-					"--glob=!**/.vscode/*",
-					"--glob=!**/build/*",
-					"--glob=!**/dist/*",
-					"--glob=!**/yarn.lock",
-					"--glob=!**/package-lock.json",
-				},
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-					-- needed to exclude some files & dirs from general search
-					-- when not included or specified in .gitignore
-					find_command = {
-						"rg",
-						"--files",
-						"--hidden",
-						"--glob=!**/.git/*",
-						"--glob=!**/.idea/*",
-						"--glob=!**/.vscode/*",
-						"--glob=!**/build/*",
-						"--glob=!**/dist/*",
-						"--glob=!**/yarn.lock",
-						"--glob=!**/package-lock.json",
-					},
-				},
 			},
 		})
 
@@ -68,6 +30,7 @@ return {
 
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
--- .zshrc
+++ .zshrc
@@ -17,11 +17,47 @@ source ~/.zsh_extra 2> /dev/null
 
 source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+# fzf config
+
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+_fzf_compgen_path() {
+  fd --hidden --exclude .git . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type=d --hidden --exclude .git . "$1"
+}
+
+source ~/.config/fzf/fzf-git.sh
+
+show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
+
+export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo \${}'"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+  esac
+}
+
 source <(fzf --zsh)
+
+# end fzf config
 
 eval "$(starship init zsh)"
 
