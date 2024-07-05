dc6d35c5f1a5ca80b07e05df8e2a8e0bc4323ed2
eladmz <eladmz@gmail.com>
2024-07-05 20:50:15 +0300

    use delta in lazygit

--- .config/nvim/lua/eladmz/plugins/telescope.lua
+++ .config/nvim/lua/eladmz/plugins/telescope.lua
@@ -60,17 +60,5 @@ return {
 		})
 
 		telescope.load_extension("fzf")
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		-- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		-- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		-- keymap.set("n", "<C-p>", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git files" })
-		-- keymap.set("n", "<leader>fg", "<cmd>Telescope git_status<cr>", { desc = "Fuzzy find git dirty files" })
-		-- keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find buffers" })
-		-- keymap.set("n", "<leader>fs", "<cmd>Telescope git_grep<cr>", { desc = "Find string in cwd" })
-		-- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		-- keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
