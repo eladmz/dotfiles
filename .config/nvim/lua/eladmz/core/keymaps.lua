a291918c4c1d3291392900330f919046b0bd9772
eladmz <eladmz@gmail.com>
2024-07-06 23:03:07 +0300

    remove bufferline and tabs

--- .config/nvim/lua/eladmz/core/keymaps.lua
+++ .config/nvim/lua/eladmz/core/keymaps.lua
@@ -13,13 +13,6 @@ keymap.set("n", "<leader>wx", "<C-W>c", { desc = "Delete Window", remap = true }
 keymap.set("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
 keymap.set("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
 
--- tab management
-keymap.set("n", "<leader>tc", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tl", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>th", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tt", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
 -- Resize window using <ctrl> arrow keys
 keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
@@ -29,6 +22,7 @@ keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase W
 -- buffers
 keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+keymap.set("n", "<leader>bx", "<cmd>bd<cr>", { desc = "Close Buffer" })
 
 -- Clear search with <esc>
 keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
