1646bf5c77ca47c6402a239367711c1e19f78da0
eladmz <eladmz@gmail.com>
2024-07-10 22:15:21 +0300

    change tab size to 4

--- .config/nvim/lua/eladmz/core/options.lua
+++ .config/nvim/lua/eladmz/core/options.lua
@@ -5,8 +5,8 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
 
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
